6ec9af603b45848c416bd2311467733b9540b7a7
Filipp <afilippshestakov@gmail.com>
2026-05-26 15:52:10 +0200

    il primo passo in square board

---
+++ square_board.py
@@ -0,0 +1,22 @@
+from typing import Tuple
+Coordinate = Tuple[int, int]
+
+def circolari_in_quadro(side: int, pos: int) -> Coordinate:
+	'''
+	traduce le cordinate circolari_in_quadro(righe e colone) 
+	'''
+	circolari_range = side * 4
+	return (0, 0)
+
+
+def square_board(side: int, token: int, steps: int) -> Coordinate:
+
+    return (0, 0)
+
+if __name__ == '__main__':
+    print("Example:")
+    print(square_board(4, 1, 4))
+    assert square_board(4, 1, 4) == (1, 0)
+    assert square_board(6, 2, -3) == (4, 5)
+
+    print("Coding complete? Click 'Check' to earn cool rewards!")
