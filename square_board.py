773f07bf8d65a35029400be5af8248b55813bb89
Filipp <afilippshestakov@gmail.com>
2026-06-09 16:15:46 +0200

    add square_board

--- square_board.py
+++ square_board.py
@@ -5,17 +5,34 @@ def circolari_in_quadro(side: int, pos: int) -> Coordinate:
 	'''
 	traduce le cordinate circolari_in_quadro(righe e colone) 
 	'''
-	circolari_range = side * 4
-	return (0, 0)
+	
+	if pos <= (side - 1) * 1 - 1:
+		return (side - 1, side - pos - 1)
+	elif pos <= (side - 1) * 2 - 1:
+		return (side - 1 - pos % (side - 1), 0)
+	elif pos <= (side - 1) * 3 - 1:
+		return (0, pos % (side - 1))
+
+	else:
+		return (pos % (side - 1), side - 1)
+
+
 
 
 def square_board(side: int, token: int, steps: int) -> Coordinate:
-
-    return (0, 0)
+	circolari_range = (side - 1) * 4
+	pos_new = (token + steps) % circolari_range
+	if pos_new < 0:
+		pos_new = pos_new + circolari_range
+	return circolari_in_quadro(side, pos_new)
 
 if __name__ == '__main__':
     print("Example:")
-    print(square_board(4, 1, 4))
+    print(square_board(10, 7, 0))
+    assert square_board(10,7,36) == (9, 2)
+    assert circolari_in_quadro(4, 2) == (3, 1)
+    assert circolari_in_quadro(6, 6) == (4, 0)
+    assert circolari_in_quadro(6, 16) == (1, 5)
     assert square_board(4, 1, 4) == (1, 0)
     assert square_board(6, 2, -3) == (4, 5)
 
