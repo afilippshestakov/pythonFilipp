ccccc4ee07421d9d2ffda1426942e96bd42981a6
Filipp <afilippshestakov@gmail.com>
2026-06-26 11:12:33 +0200

    add 2 files

---
+++ liste.py
@@ -0,0 +1,8 @@
+"""
+Crea una lista vuota e aggiungici 10 numeri casuali e stampali. In questo esercizio è necessario utilizzare la funzione randint.
+
+from random import randint
+n = randint(1, 10) # Numero casuale da 1 a 10
+
+4.Rimuovi tutti gli elementi dalla lista creata nell'esercizio 3
+"""
---
+++ while.py
@@ -0,0 +1,34 @@
+# Da A a B divisibili per tre
+# L'utente inserisce i numeri A e B (A < B, A minore di B). Stampa i numeri da A a B inclusi, che sono divisibili per tre
+# a = int(input("inserisci il numero a: "))
+# b = int(input("inserisci il numero b: "))
+
+
+# while a <= b:
+# 	if a % 3 == 0:
+# 		print(a)
+#	a = a + 1
+
+# Somma dei numeri
+# L'utente inserisce numeri finché non inserisce 0. Stampa la somma dei numeri inseriti (0 non deve essere contato).
+# somma = 0
+# while True:
+# 	numero = int(input("inserisci i numero (0 per fermarti): "))
+	
+# 	if numero == 0:
+# 		break
+# 	somma = somma + numero
+# print(f"Hai inserito come somma: {somma}")
+
+# Massimo
+# L'utente inserisce numeri finché non inserisce 0. Stampa il numero massimo inserito (0 non deve essere contato).
+max_numero = 0
+while True:
+	numero = int(input("inserisci i numero (0 per fermarti): "))
+
+	if numero == 0:
+		break
+	if numero > max_numero:
+		max_numero = numero
+	if
+print(f"il numero massimo e: {max_numero}")
